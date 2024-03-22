495a1b4022e178f4f3bef6eb02d9ae6dfc2d4ba9
hoonglily <hoonglily4@gmail.com>
2024-03-21 20:20:29 -0700

    Got playRound function to return correct messages when a user wins or loses

--- rps.js
+++ rps.js
@@ -10,12 +10,24 @@ const getComputerChoice = () => {
 };
 
 const playRound = (playerSelection, computerSelection) => {
-  if (playerSelection === computerSelection) {
-    return 'Tie! Please go again'
+  const lowerCasePS = playerSelection.toLowerCase();
+
+  const outcomes = {
+    rock: { beats: 'scissors', message: 'Rock beats Scissors' },
+    paper: { beats: 'rock', message: 'Paper beats Rock' },
+    scissors: { beats: 'paper', message: 'Scissors beats Paper' }
   };
+
+  if (lowerCasePS === computerSelection) {
+    return 'Tie! Please go again';
+  } else if (outcomes[lowerCasePS].beats === computerSelection){
+    return `You win! ${outcomes[lowerCasePS].message}`;
+  } else {
+    return `You Lose! ${outcomes[computerSelection].message}`;
+  }   
 };
 
-const playerSelection = 'rock';
+const playerSelection = 'scissors';
 const computerSelection = getComputerChoice();
-console.log(computerSelection);
+console.log('CPU:', computerSelection);
 console.log(playRound(playerSelection, computerSelection));
